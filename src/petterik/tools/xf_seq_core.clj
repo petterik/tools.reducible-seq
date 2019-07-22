f9847997f8e30fe791dd0a5801d09239d922b165
Petter Eriksson <ipetterik@gmail.com>
2019-07-21 21:29:01 -0700

    Made single fn xf-seq smaller and uses case

--- src/petterik/tools/xf_seq_core.clj
+++ src/petterik/tools/xf_seq_core.clj
@@ -58,55 +58,49 @@
               (step s (xform arr-conj!) (java.util.ArrayList. 4)))))))))
 
 (def ^:static xf-seq-2
-  (let [step (clojure.lang.Var/create)
-
-        end-cons (fn [^java.util.ArrayList buf]
-                   (let [size (.size buf)]
-                     (case* size 0 0
-                       ;; else
-                       (clojure.lang.ChunkedCons. (clojure.lang.ArrayChunk. (.toArray buf)) nil)
-                       ;; cases
-                       {0 [0 nil]
-                        1 [1 (cons (.get buf 0) nil)]}
-                       :compact
-                       :int)))
-
-        step* (fn [s xf ^java.util.ArrayList buf]
-                (let [chunked? (chunked-seq? s)
-                      ret (if chunked?
-                                (.reduce (chunk-first s) xf buf)
-                                (xf buf (first s)))]
-                  (if (clojure.lang.RT/isReduced ret)
-                    (end-cons (xf (.deref ^clojure.lang.Reduced ret)))
-                    (let [s (if chunked? (chunk-rest s) (rest s))
-                          size (.size buf)]
-                      (case* size 0 0
-                        ;; else
-                        (clojure.lang.ChunkedCons.
-                          (clojure.lang.ArrayChunk. (.toArray buf))
-                          (lazy-seq (step s xf buf)))
-                        ;; cases
-                        {0 [0 (let [s (seq s)]
-                                (if s (recur s xf buf) (end-cons (xf buf))))]
-                         1 [1 (cons (.get buf 0) (lazy-seq (step s xf buf)))]}
-                        :compact
-                        :int)))))
-
-        arr-conj! (fn
+  (let [arr-conj! (fn
                     ([] (java.util.ArrayList.))
                     ([buf] buf)
                     ([^java.util.ArrayList buf x]
                      (.add buf x)
-                     buf))]
-    (.bindRoot step (fn [s xf buf]
-                      (.clear ^java.util.ArrayList buf)
-                      (let [s (seq s)]
-                        (if s
-                          (step* s xf buf)
-                          (end-cons (xf buf))))))
-    (let [step (.deref step)]
-      (fn [xform coll]
-        (lazy-seq
-          (let [s (seq coll)]
-            (when s
-              (step s (xform arr-conj!) (java.util.ArrayList. 4)))))))))
+                     buf))
+        step (fn self [s xf ^java.util.ArrayList buf]
+               (if s
+                 (let [chunked? (chunked-seq? s)
+                       ret (if chunked?
+                             (.reduce (chunk-first s) xf buf)
+                             (xf buf (first s)))]
+                   (if (clojure.lang.RT/isReduced ret)
+                     ;; TODO: is the reduced ret always buf?
+                     (recur nil xf (.deref ^clojure.lang.Reduced ret))
+                     (let [s (if chunked? (chunk-rest s) (rest s))
+                           size (.size buf)]
+                       (case* size 0 0
+                         ;; else
+                         (clojure.lang.ChunkedCons. (clojure.lang.ArrayChunk. (.toArray buf))
+                           (lazy-seq
+                             (.clear buf)
+                             (self (seq s) xf buf)))
+                         ;; cases
+                         {0 [0 (recur (seq s) xf buf)]
+                          1 [1 (cons (.get buf 0)
+                                 (lazy-seq
+                                   (.clear buf)
+                                   (self (seq s) xf buf)))]}
+                         :compact
+                         :int))))
+                 (let [^java.util.ArrayList buf (xf buf)
+                       size (.size buf)]
+                   (case* size 0 0
+                     ;; else
+                     (clojure.lang.ChunkedCons. (clojure.lang.ArrayChunk. (.toArray buf)) nil)
+                     ;; cases
+                     {0 [0 nil]
+                      1 [1 (cons (.get buf 0) nil)]}
+                     :compact
+                     :int))))]
+    (fn [xform coll]
+      (lazy-seq
+        (let [s (seq coll)]
+          (when s
+            (step s (xform arr-conj!) (java.util.ArrayList. 4))))))))
